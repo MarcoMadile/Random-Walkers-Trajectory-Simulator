fb452c845b1ac28fc8fa936ba7980664ab1c88c9
MarcoMadile <marco.madile@ib.edu.ar>
2023-06-25 12:58:39 -0300

    Now running on GPU evolution

--- GPU_code.py
+++ GPU_code.py
@@ -1,18 +1,26 @@
 import cupy as cp
-import cupyx as cpx
+#import cupyx as cpx
 import ipdb
-import numpy as np
 from scipy.spatial import distance_matrix
 from matplotlib import pyplot as plt
 import networkx as nx
 
+def distance_matrix(a, b):
+    a2 = cp.sum(a*a, axis=1)
+    b2 = cp.sum(b*b, axis=1)
+    ab = cp.dot(a, b.T)
+    dists = cp.sqrt(a2[:, None] - 2*ab + b2[None, :])
+    return dists
+
+
+
 class Individual:
     def __init__(self, initial_position, waiting_time_dist, step_size_dist):
-        self.ini_position = np.array(initial_position)
-        self.waiting_time_hist = waiting_time_dist[0]/np.sum(waiting_time_dist[0])
+        self.ini_position = cp.array(initial_position)
+        self.waiting_time_hist = waiting_time_dist[0]/cp.sum(waiting_time_dist[0])
         self.waiting_time_edges = (waiting_time_dist[1][:-1]+waiting_time_dist[1][1:])/2
-        self.waiting_time_mean =  np.sum(self.waiting_time_edges * self.waiting_time_hist)
-        self.step_size_hist = step_size_dist[0]/np.sum(step_size_dist[0])
+        self.waiting_time_mean =  cp.sum(self.waiting_time_edges * self.waiting_time_hist)
+        self.step_size_hist = step_size_dist[0]/cp.sum(step_size_dist[0])
         self.step_size_edges = (step_size_dist[1][:-1]+step_size_dist[1][1:])/2
         self.time = 0
         self.positions= [self.ini_position]
@@ -21,40 +29,40 @@ class Individual:
     def generate_waiting_time(self,size_=1):
         # Generate a random waiting time based on the waiting_time_dist
         # Replace the function below with your desired waiting time distribution function
-        return np.random.choice(self.waiting_time_edges,size=size_, p=self.waiting_time_hist)
+        return cp.random.choice(self.waiting_time_edges,size=size_, p=self.waiting_time_hist)
 
     def generate_step_size(self,size_=1):
         # Generate a random step size based on the step_size_dist
         # Replace the function below with your desired step size distribution function
-        return np.random.choice(self.step_size_edges,size=size_, p=self.step_size_hist)
+        return cp.random.choice(self.step_size_edges,size=size_, p=self.step_size_hist)
 
     def move(self,time):
         # Generate a random direction
-        positions= np.zeros((2,time))
+        positions= cp.zeros((2,time))
         # get mean of waiting time distribution 
         posible_size_w_t = int(2*time/self.waiting_time_mean)  
         waiting_time= self.generate_waiting_time(size_=posible_size_w_t)                                              
-        while np.sum(waiting_time)<=time: 
-            waiting_time = np.hstack((waiting_time,self.generate_waiting_time(size_=posible_size_w_t)))
-        cumulative_sum = np.cumsum(waiting_time)
-        index = np.where(cumulative_sum > time)[0][0]
+        while cp.sum(waiting_time)<=time: 
+            waiting_time = cp.hstack((waiting_time,self.generate_waiting_time(size_=posible_size_w_t)))
+        cumulative_sum = cp.cumsum(waiting_time)
+        index = cp.where(cumulative_sum > time)[0][0]
         filtered_waiting_time = waiting_time[:index]
         step_size = self.generate_step_size(size_=len(filtered_waiting_time))
-        theta = np.random.uniform(0, 2 * np.pi,size=len(filtered_waiting_time))
-        direction = [np.cos(theta), np.sin(theta)]
+        theta = cp.random.uniform(0, 2 * cp.pi,size=len(filtered_waiting_time))
+        direction = [cp.cos(theta), cp.sin(theta)]
         # Update position based on this simple model where individual stays still for each waiting time (updating positions with the actual position) and when the waiting time finishes move with step size in direction. 
         filtered_waiting_time = filtered_waiting_time.astype(int)
         # check if it has 0 in the waiting time 
         if 0 in filtered_waiting_time:
-            filtered_waiting_time= filtered_waiting_time+np.ones(len(filtered_waiting_time),dtype=int)
+            filtered_waiting_time= filtered_waiting_time+cp.ones(len(filtered_waiting_time),dtype=int)
         # Generate movements for each waiting time
-        movements = step_size[:, np.newaxis] * np.array(direction).T
+        movements = step_size[:, cp.newaxis] * cp.array(direction).T
         # Repeat each movement for the corresponding waiting time
-        repeated_movements = np.repeat(movements, filtered_waiting_time, axis=0)
+        repeated_movements = cp.repeat(movements, filtered_waiting_time.tolist(), axis=0)
         # add initial position to repeated_movements
-        repeated_movements = np.vstack((self.ini_position,repeated_movements))
+        repeated_movements = cp.vstack((self.ini_position,repeated_movements))
         # Calculate positions
-        positions = np.cumsum(repeated_movements, axis=0)[:time]
+        positions = cp.cumsum(repeated_movements, axis=0)[:time]
         # Update positions
         self.positions=positions    
 
@@ -71,15 +79,16 @@ class Ensemble:
         for i in range(self.n):
             self.individuals[i].move(time)
 
+    #need to change this function to gpu or something 
     def find_intersections(self, t0, x0):
-        positions = np.array([ind.positions for ind in self.individuals])
+        positions = cp.array([ind.positions for ind in self.individuals])
         n_individuals, time, _ = positions.shape
         intersections = []
         for i in range(n_individuals):
             for j in range(i+1, n_individuals):
                 # Find the time indices where the individuals are close to each other
                 dist = distance_matrix(positions[i], positions[j])
-                close_indices = np.where((dist < x0) & (np.abs(np.subtract.outer(np.arange(time), np.arange(time))) < t0))
+                close_indices = cp.where((dist < x0) & (cp.abs(cp.subtract.outer(cp.arange(time), cp.arange(time))) < t0))
                 if len(close_indices[0]) > 0:
                     # Add the intersection times and the individual indices to the list of intersections
                     intersection_times_i = close_indices[0]
@@ -88,10 +97,10 @@ class Ensemble:
                         intersections.append((i, j, intersection_times_i[k], intersection_times_j[k]))
         return intersections
       
-    def plot(self,intersections):
+    def plot(self,plot_intesections=True,intersections=[]):
         fig, ax = plt.subplots()
         for i in range(self.n):
-            ax.plot(self.individuals[i].positions[:, 0], self.individuals[i].positions[:, 1], 'o-')
+            ax.plot(self.individuals[i].positions[:, 0].get(), self.individuals[i].positions[:, 1].get(), 'o-')
         for i, j, time1,time2 in intersections:
 
             # plot a line from individual i to individual j
@@ -111,16 +120,15 @@ class Ensemble:
         plt.show()
 
 
-    
 # Generate random numbers from an exponential distribution
-data = np.random.exponential(scale=1.0, size=1000)
+data = cp.random.exponential(scale=1.0, size=1000)
 # Create a histogram of the data
-hist, bin_edges = np.histogram(data, bins=10, density=True)
+hist, bin_edges = cp.histogram(data, bins=10, density=True)
 #ind = Individual(initial_position=[0,0],waiting_time_dist=[hist,bin_edges],step_size_dist=[hist,bin_edges])
 time = 2*60
 #ind.move(time=time)
 n_ind= 5
-initial_positions= np.zeros((n_ind,2))
+initial_positions= cp.zeros((n_ind,2))
 population= Ensemble(n_ind,initial_positions,waiting_time_dist=[hist,bin_edges],step_size_dist=[hist,bin_edges])
 population.move(time)
 intersections= population.find_intersections(t0=5,x0=2)
